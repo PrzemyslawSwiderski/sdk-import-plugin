7fe019a83924550865b41ad06459c43bbb8924ba
PrzemyslawSwiderski <przemekzgierz7@gmail.com>
2025-01-25 03:11:42 +0100

    chore: fixed Windows local tests

--- src/test/kotlin/com/pswidersk/sdkimportplugin/customFixtures.kt
+++ src/test/kotlin/com/pswidersk/sdkimportplugin/customFixtures.kt
@@ -3,11 +3,13 @@ package com.pswidersk.sdkimportplugin
 import com.intellij.ide.impl.OpenProjectTask
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ex.ProjectManagerEx
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.testFramework.junit5.fixture.TestFixture
 import com.intellij.testFramework.junit5.fixture.testFixture
 import java.nio.file.Files
 
 fun customProjectFixture(): TestFixture<Project> = testFixture {
+    Registry.get("wsl.use.remote.agent.for.nio.filesystem").setValue(false)
     val openProjectTask = OpenProjectTask.build()
     val path = Files.createTempDirectory("IJ")
     val project = ProjectManagerEx.getInstanceEx().newProjectAsync(path, openProjectTask)
